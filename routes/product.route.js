7c521ec015fbd244cb848d4be10dbf258aeef380
eemurphy <erinmurphy897@gmail.com>
2019-03-04 22:58:30 -0500

    trying not local

--- routes/product.route.js
+++ routes/product.route.js
@@ -11,3 +11,6 @@ router.post('/create', product_controller.product_create);
 router.get('/:id', product_controller.product_details);
 router.put('/:id/update', product_controller.product_update);
 router.delete('/:id/delete', product_controller.product_delete);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
