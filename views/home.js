2232a950c3f83d91ed8f55d7630b4484a44c0a5a
eemurphy <erinmurphy897@gmail.com>
2019-03-10 14:08:34 -0400

    add date to page

--- views/home.js
+++ views/home.js
@@ -1,15 +1,2 @@
-let d = new Date();
-alert("Today's date is " + d);
-
-fetch('/test', {method: 'GET'})
-    .then(function(response) {
-      if(response.ok) return response.json();
-      throw new Error('Request failed.');
-    });
-    .then(function(data) {
-      document.getElementById('name').innerHTML = `Button was clicked ${data.length} times`;
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-}, 1000);
+var d = new Date();
+document.getElementById("demo").innerHTML = "Today's date is " + d.toDateString();
